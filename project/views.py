2f433f0c90c2106d87ebd2e541d6afb171bb55f9
Alex Clark <aclark@aclark.net>
2022-03-09 09:36:59 -0500

    Update

--- project/views.py
+++ project/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.views.generic import DetailView, ListView
+from django.core import serializers
+
 
 
 class UserListView(ListView):
@@ -8,3 +10,13 @@ class UserListView(ListView):
 
 class UserDetailView(DetailView):
     model = User
+
+    # https://docs.djangoproject.com/en/4.0/topics/class-based-views/generic-display/#adding-extra-context
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+
+        fields = self.object._meta.get_fields()
+        context["fields"] = fields
+        
+        return context
